33c9bcf30671141dc8265eb1c979deec93526a4a
bhavana-vempati <bhavanavempati455@gmail.com>
2020-10-14 12:34:27 +0530

    my commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { ExcelServiceService } from "./excel-service.service";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'download-excel';
+  title = "download-excel";
+  name = "Angular 6";
+  data: any = [
+    {
+      eid: "e101",
+      ename: "ravi",
+      esal: 1000,
+    },
+    {
+      eid: "e102",
+      ename: "ram",
+      esal: 2000,
+    },
+    {
+      eid: "e103",
+      ename: "rajesh",
+      esal: 3000,
+    },
+  ];
+  constructor(private excelService: ExcelServiceService) {}
+  exportAsXLSX(): void {
+    this.excelService.exportAsExcelFile(this.data, "sample");
+  }
 }
